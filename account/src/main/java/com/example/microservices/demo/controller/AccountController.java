a4c09866f01ea0e333165001a588cdb97c965d2e
stephenman <stephenman@cherrypicks.com>
2019-08-30 18:22:00 +0800

    initial commit

---
+++ account/src/main/java/com/example/microservices/demo/controller/AccountController.java
@@ -0,0 +1,43 @@
+package com.example.microservices.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.microservices.demo.entity.Account;
+import com.example.microservices.demo.service.AccountService;
+
+@RestController
+@RequestMapping("/")
+public class AccountController {
+	@Autowired
+	private AccountService accountService;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Account> getAccountById(@PathVariable long id) {
+		Account account = this.accountService.getAccountById(id).orElse(null);
+		
+		return new ResponseEntity<>(account, HttpStatus.OK);
+	}
+	
+	@GetMapping
+	public ResponseEntity<Account> getAccountByAccountNo(@RequestParam(name="accountNo") String accountNo) {
+		Account account = this.accountService.getAccountByAccountNo(accountNo).orElse(null);
+		
+		return new ResponseEntity<>(account, HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Object> openAccount(@RequestBody Account account) {
+		Account result = this.accountService.saveAccount(account);
+		
+		return new ResponseEntity<>(result, HttpStatus.CREATED);
+	}
+}
