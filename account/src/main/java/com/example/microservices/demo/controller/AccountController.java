8bde7086d8a2960a0db530d7b4573f32aaa5ac4e
stephenman <stephenman@cherrypicks.com>
2019-08-30 18:44:20 +0800

    added logging

--- account/src/main/java/com/example/microservices/demo/controller/AccountController.java
+++ account/src/main/java/com/example/microservices/demo/controller/AccountController.java
@@ -1,5 +1,7 @@
 package com.example.microservices.demo.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +19,14 @@ import com.example.microservices.demo.service.AccountService;
 @RestController
 @RequestMapping("/")
 public class AccountController {
+	private static final Logger logger = LogManager.getLogger(AccountController.class);
+	
 	@Autowired
 	private AccountService accountService;
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Account> getAccountById(@PathVariable long id) {
+		logger.debug("getAccountById - id: {}", () -> id);
 		Account account = this.accountService.getAccountById(id).orElse(null);
 		
 		return new ResponseEntity<>(account, HttpStatus.OK);
@@ -29,6 +34,7 @@ public class AccountController {
 	
 	@GetMapping
 	public ResponseEntity<Account> getAccountByAccountNo(@RequestParam(name="accountNo") String accountNo) {
+		logger.debug("getAccountByAccountNo - accountNo: {}", () -> accountNo);
 		Account account = this.accountService.getAccountByAccountNo(accountNo).orElse(null);
 		
 		return new ResponseEntity<>(account, HttpStatus.OK);
@@ -36,6 +42,8 @@ public class AccountController {
 	
 	@PostMapping
 	public ResponseEntity<Object> openAccount(@RequestBody Account account) {
+		logger.debug("openAccount - account: {}", () -> account);
+		
 		Account result = this.accountService.saveAccount(account);
 		
 		return new ResponseEntity<>(result, HttpStatus.CREATED);
